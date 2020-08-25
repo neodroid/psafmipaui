7ff9b19c446cd1679babdd112c7a526c567be240
kevin ahmad <kevinahmad.f@gmail.com>
2020-08-26 00:38:47 +0700

    fix bug

--- pages/component/header.tsx
+++ pages/component/header.tsx
@@ -7,108 +7,105 @@ const MenuItems = ({ children }) => (
     "5", // 480px upwards
     "5", // 768px upwards
     "0", // 992px upwards
-]} 
-textAlign="justify"
-mr={6} display="block">
+  ]}
+    textAlign="justify"
+    mr={6} display="block">
     {children}
   </Text>
 );
 
 // Note: This code could be better, so I'd recommend you to understand how I solved and you could write yours better :)
-const Header = props => {
+const Header = () => {
   const [show, setShow] = React.useState(false);
   const handleToggle = () => setShow(!show);
 
   return (
     <Flex
-    w="100%"
-    bg="#EAF2FD"
-    align="center"
-    justify="center"
-    pos="fixed" top="0" left="0"
-    overflow="hidden"
-    zIndex={2}
->
-    <Flex
-       width={[
-        "100%", // base
-        "100%", // 480px upwards
-        "100%", // 768px upwards
-        "75%", // 992px upwards
-    ]}
-      as="nav"
-      align="center"
-      justify="space-between"
-      wrap="wrap"
-      padding="1.5rem"
+      w="100%"
       bg="#EAF2FD"
-      color="#2F80ED"
-      
-     
-      {...props}
+      align="center"
+      justify="center"
+      pos="fixed" top="0" left="0"
+      overflow="hidden"
+      zIndex={2}
     >
-      <Flex align="center" mr={5}  justifyContent="center" 
-     >
-        <Heading as="h1" size="lg" letterSpacing={"-.1rem"}>
+      <Flex
+        width={[
+          "100%", // base
+          "100%", // 480px upwards
+          "100%", // 768px upwards
+          "75%", // 992px upwards
+        ]}
+        as="nav"
+        align="center"
+        justify="space-between"
+        wrap="wrap"
+        padding="1.5rem"
+        bg="#EAF2FD"
+        color="#2F80ED"
+      >
+        <Flex align="center" mr={5} justifyContent="center"
+        >
+          <Heading as="h1" size="lg" letterSpacing={"-.1rem"}>
             <Box
-             width={[
+              width={[
                 "40px", // base
                 "40px", // 480px upwards
                 "50px", // 768px upwards
                 "85px", // 992px upwards
               ]}
-            ><img src="/logo.png"/></Box>
-         
-        </Heading>
-       
-      </Flex>
+            ><img src="/logo.png" /></Box>
 
-      <Box 
-      display={[
-        "block", // base
-        "block", // 480px upwards
-        "block", // 768px upwards
-        "none", // 992px upwards
-      ]}
-      onClick={handleToggle}>
-        <svg
-          fill="black"
-          width="20px"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
+          </Heading>
+
+        </Flex>
+
+        <Box
+          display={[
+            "block", // base
+            "block", // 480px upwards
+            "block", // 768px upwards
+            "none", // 992px upwards
+          ]}
+          onClick={handleToggle}>
+          <svg
+            fill="black"
+            width="20px"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </Box>
+
+        <Flex
+          display={[
+            show ? "block" : "none", // base
+            show ? "block" : "none", // 480px upwards
+            show ? "block" : "none", // 768px upwards
+            "flex", // 992px upwards
+          ]}
+          width={[
+            "full", // base
+            "full", // 480px upwards
+            "full", // 768px upwards
+            "auto", // 992px upwards
+          ]}
+          pt="5"
+
+          flexGrow={1}
+          textAlign="justify"
+          flexDirection="row-reverse"
         >
-          <title>Menu</title>
-          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-        </svg>
-      </Box>
+          <MenuItems>TUGAS</MenuItems>
+          <MenuItems>MENTORING</MenuItems>
+          <MenuItems>MEDIA</MenuItems>
+          <MenuItems>DEPARTEMEN</MenuItems>
+          <MenuItems>PROFIL</MenuItems>
+          <MenuItems>HOME</MenuItems>
 
-      <Flex
-        display={[
-          show ? "block" : "none", // base
-          show ? "block" : "none", // 480px upwards
-          show ? "block" : "none", // 768px upwards
-          "flex", // 992px upwards
-        ]}
-        width={[
-          "full", // base
-          "full", // 480px upwards
-          "full", // 768px upwards
-          "auto", // 992px upwards
-        ]}
-        pt="5"
-        
-        flexGrow={1}
-        textAlign="justify"
-        flexDirection="row-reverse"
-      >
-        <MenuItems>TUGAS</MenuItems>
-        <MenuItems>MENTORING</MenuItems>
-        <MenuItems>MEDIA</MenuItems>
-        <MenuItems>DEPARTEMEN</MenuItems>
-        <MenuItems>PROFIL</MenuItems>
-        <MenuItems>HOME</MenuItems>
-        
-      </Flex>
+        </Flex>
 
       </Flex>
     </Flex>
